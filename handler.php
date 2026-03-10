4a82d4db03edb3f55d49314074ce69bf3e4949bc
Pavel Kapitonov <161024801+Pavel-Kapitonov@users.noreply.github.com>
2026-03-10 18:39:01 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,11 +1,24 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
-echo "<h1>Данные успешно доелетли до бэкенда!</h1>";
+$fio = $_POST['fio'] ?? '';
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+$errors = [];
+
+if (empty($fio)) {
+    $errors = "Поле ФИО пустое";
+} elseif (mb_strlen($fio) > 150) {
+    $errors = "ФИО слишком длинное"
+} elseif (!preg_match('/^[a-zA-Zа-яёА-ЯЁ ]+$/u', $fio)) {
+    $errors[] = "В  ФИО можно только буквы и пробелы"
+}
+
+if (!empty($errors)) {
+    echo "<h2>Произошли ошибки:</h2>";
+    foreach ($errors as $errors) {
+        echo "- $error<br>";
+    }
+} else {
+    echo "<h2>Данные прошли проверку</h2>"
+}
+
 ?>
-
